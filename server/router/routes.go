1682e1e3aec9f65dc28db988762986dd4770feb9
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-11-04 14:51:13 -0300

    Update routes.go
    Co-Authored-By: Amanda <limaa9000@gmail.com>

--- server/router/routes.go
+++ server/router/routes.go
@@ -5,6 +5,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func initializeRoutes(r *gin.Engine) {
-	r.GET("/list-flights", handler.ListFlightsHandler)
+func InitializeRoutes(r *gin.Engine, externalCompanies []string) {
+	r.GET("/list-flights", func(c *gin.Context) {
+		handler.ListFlightsHandler(c, externalCompanies)
+	})
+	r.POST("/reserve-seat", func(c *gin.Context) {
+		handler.ReserveSeatHandler(c, externalCompanies)
+	})
+	r.POST("/register-client", handler.RegisterClientHandler)
+	r.POST("/cancel-reservation", func(c *gin.Context) {
+		handler.CancelReservationHandler(c, externalCompanies)
+	})
 }
