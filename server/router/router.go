303b6cb3d31887566d8b973bc9a371e60f4e0fea
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-11-06 11:40:35 -0300

    feat: insere cors
    Co-Authored-By: Amanda <limaa9000@gmail.com>

--- server/router/router.go
+++ server/router/router.go
@@ -2,11 +2,13 @@ package router
 
 import (
     "fmt"
+    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
 )
 
 func Initialize(externalCompanies []string, port int) {
     r := gin.Default()
+    r.Use(cors.Default())
     InitializeRoutes(r, externalCompanies)
     r.Run(fmt.Sprintf(":%d", port)) // Escuta na porta configurada
 }
